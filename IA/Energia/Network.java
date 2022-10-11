5506146bb107aa4b1027cb967f56c0d6e6786011
JCasahuga <joan.casahuga.altimiras@gmail.com>
2022-10-11 17:20:14 +0200

    Updated Network

--- IA/Energia/Network.java
+++ IA/Energia/Network.java
@@ -1,13 +1,6 @@
 package IA.Energia;
 
 public class Network {
-    /* Class independent from AIMA classes
-       - It has to implement the state of the problem and its operators
-     *
-
-    /* State data structure
-        vector with the parity of the coins (we can assume 0 = heads, 1 = tails
-     */
 
     private int [] board;
     private static int [] solution;
@@ -18,7 +11,7 @@ public class Network {
         board = new int[init.length];
         solution = new int[init.length];
 
-        for (int i = 0; i< init.length; i++) {
+        for (int i = 0; i < init.length; i++) {
             board[i] = init[i];
             solution[i] = goal[i];
         }
@@ -38,7 +31,6 @@ public class Network {
 
     /* Heuristic function */
     public double heuristic(){
-        // compute the number of coins out of place respect to solution
         return 0;
     }
 
