53b6f05d8474bb6f4f126adb3b2d416b55acc6ef
miquel.torner.vinals <miquel.torner.vinals@estudiantat.upc.edu>
2022-10-12 11:38:37 +0200

    "successors"

---
+++ IA/Energia/ElectricalNetworkSuccesorFunctionHillClimbing.java
@@ -0,0 +1,37 @@
+package IA.Energia;
+
+import IA.Energia.*;
+
+import aima.search.framework.SuccessorFunction;
+import aima.search.framework.Successor;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ElectricalNetworkSuccesorFunctionHillClimbing implements SuccessorFunction{
+
+    public List getSuccessors(Object state){
+		ArrayList <Successor> retval = new ArrayList<>();
+    	ElectricalNetworkState networkState = new ElectricalNetworkState ((ElectricalNetworkState) state);
+
+	
+	int totalClients = networkState.getClientsNumber();
+	int totalCentrals = networkState.getCentralsNumber();
+	
+	for (int i = 0; i < totalClients; ++i) {
+		for (int j = 0; j < totalCentrals; ++j) {
+		    ElectricalNetworkState new_state = new ElectricalNetworkState(networkState); 
+		    new_state.mouClient(i, j);
+		    retval.add(new Successor("actionName", new_state));
+		}
+	}
+	
+	for (int i = 0; i < totalClients; ++i) {
+		for (int j = 0; j < totalClients; ++j) {
+		    ElectricalNetworkState new_state = new ElectricalNetworkState(networkState);
+		    new_state.swapClient(i, j);
+		    retval.add(new Successor("actionName", new_state));
+		}
+	}
+        return retval;
+    }
+}
