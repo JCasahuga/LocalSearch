1164127807f19e2ee954081fbd4ecc4b5f3c0cf3
xavier.bernat.lopez <xavierbern2@gmail.com>
2022-10-13 13:03:40 +0200

    reactivated swap in getSuccessors

--- IA/Energia/ElectricalNetworkSuccesorFunctionHillClimbing.java
+++ IA/Energia/ElectricalNetworkSuccesorFunctionHillClimbing.java
@@ -28,13 +28,13 @@ public class ElectricalNetworkSuccesorFunctionHillClimbing implements SuccessorF
 		}
 	}
 	
-	// for (int i = 0; i < totalClients; ++i) {
-	// 	for (int j = 0; j < totalClients; ++j) {
-	// 	    ElectricalNetworkState new_state = new ElectricalNetworkState(networkState);
-	// 	    new_state.swapClient(i, j);
-	// 	    retval.add(new Successor("actionName", new_state));
-	// 	}
-	// }
+	   for (int i = 0; i < totalClients; ++i) {
+	 	for (int j = 0; j < totalClients; ++j) {
+	 	    ElectricalNetworkState new_state = new ElectricalNetworkState(networkState);
+	 	    new_state.swapClient(i, j);
+	  	    retval.add(new Successor("actionName", new_state));
+	   	}
+	    }
         return retval;
     }
 }
